f8ed8336a486bb01808a207e78e0484f4019e9f8
Atichie <atichie525@gmail.com>
2024-06-20 12:54:54 +0300

    Add basic_operations.py to perform basic arithmetic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+x = 10
+y = 5
+
+# perform addition, difference, product
+addition_result = x + y
+difference_result = x - y
+product_result = x * y
+
+# Print the results
+print(addition_result)
+print(difference_result)
+print(product_result)
