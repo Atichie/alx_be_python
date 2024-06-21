89a7e6d4a28b5726b6dfc2390638b537cdc04082
Atichie <atichie525@gmail.com>
2024-06-21 10:20:25 +0300

    Add finance_calculator.py to calculate the compound interest

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,22 @@
-# User Input for financial Details
+# Prompt the user for their monthly income
 monthly_income = float(input("Enter your monthly income: "))
-Total_monthly_expenses = float(input("Enter your total monthly expenses: "))
-monthly_savings = Total_monthly_expenses - monthly_income
-projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
-print(f"Your monthly savings are ${monthly_savings:.2f}.")
-print(f"Projected savings after one year, with interest, is: ${projected_savings:.2f}.")       
+
+#Prompt the user for their total monthly expenses
+monthly_expenses = float(input("Enter your total monthly expenses: "))
+
+# Calculate the monthly savings
+monthly_savings = monthly_income - monthly_expenses
+
+# Calculate the projected annual savings
+annual_savings = monthly_savings * 12
+projected_annual_savings = annual_savings + (annual_savings * 0.05)
+
+#Display the results
+print(f"Your monthly savings are ${monthly_savings:} ")  
+print(f"Projected savings after one year, with interest, is: ${projected_annual_savings:} ")   
+          
+                            
+                               
+
+                                 
+                               
