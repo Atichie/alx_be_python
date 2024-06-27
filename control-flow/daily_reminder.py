e7089ec388e0be6476131d8d66addbac17742b4b
Atichie <atichie525@gmail.com>
2024-06-27 16:26:12 +0300

    Add daily_reminder.py script to ask the user for a single task, its priority level, and if it is time sensitive

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,20 +1,20 @@
 # Prompt for a Single Task
-task = input("Enter your task: ")
-priority = input("Priority(high/medium/low): ").strip().lower()
-time_bound = input("Is it time_bound? (yes/no): ").strip().lower()
+Task = input("Enter your task: ")
+Priority = input("Priority(high/medium/low): ").strip().lower()
+Time_Bound = input("Is it time_bound? (yes/no): ").strip().lower()
 reminder = ""
 
 # Process the Task Based on Priority and Time Sensitivity
-match priority: 
+match Priority: 
     case "high":
-        reminder = f"The task '{task}' is of high priority" 
+        reminder = f"The task '{Task}' is of high priority" 
     case "medium":
-        reminder = f"The task '{task}' is of medium priority" 
+        reminder = f"The task '{Task}' is of medium priority" 
     case "low":
-        reminder = f"The task '{task}' is of low priority."
+        reminder = f"The task '{Task}' is of low priority."
     case _:
-        reminder = f"The task '{task}' has unspecified priority."
-if time_bound == "yes":
+        reminder = f"The task '{Task}' has unspecified priority."
+if Time_Bound == "yes":
     reminder += " that requires immediate attention today!"
     print(f"Reminder: {reminder}")
 else:
