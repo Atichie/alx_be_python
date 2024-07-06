fc42efaa95fb2e8447b562b7962154f8f6350124
Atichie <atichie525@gmail.com>
2024-07-06 11:41:47 -0700

    Add temp_conversion_tool.py script to calculate the temperature prompted

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,35 @@
+# Define global conversion factors
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+
+def convert_to_celsius(fahrenheit):
+    """
+    convert Fahrenheit to Celsius using the global conversion factor.
+    """
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+
+def convert_to_fahrenheit(celsius):
+    """
+    convert Celsius to Fahrenheit using the global conversion factor.
+    """
+    return celsius * CELSIUS_TO_FAHRENHEIT_FACTOR +32
+
+def main():
+    try:
+        temp = float(input("Enter the temperature: "))
+        unit = input("Is the temperature in (C)elsius or (F)ahrenheit? ").strip().upper()
+
+        if unit == 'C':
+            convert_temp = convert_to_fahrenheit(temp)
+            print(f"{temp}c is equal to {converted_temp:2f}F")
+        elif unit == 'F':
+            converted_temp = convert_to_celsius(temp)
+            print(f"{temp}F is equal to {converted_temp:sf}C")
+        else:
+            print("Invalid input. Please specify 'C' for Celsius or 'F' for Fahrenheit.")
+    except ValueError:
+        print("Invalid temperature. Please enter a numeric value.")
+
+name = 'main'
+if name == '_main_':
+    main()
