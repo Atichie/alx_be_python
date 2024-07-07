6f5c54c1a9ca0270dc70c5d45b05da79e1ed7856
Atichie <atichie525@gmail.com>
2024-07-07 03:34:21 -0700

    Add arithmetic_operations.py script to calculate the basic arithmetic operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,17 +1,17 @@
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
-operation = input("Enter the operation (add,subtract, multiply, division): ")
-def perform_operation(add, subtract, multiply, division):
+operation = input("Enter the operation (add, subtract, multiply, divide): ")
+def perform_operation(num1, num2, operation):
     if operation == 'add':
         return num1 +num2
     elif operation == 'subtract':
         return num1 - num2
     elif operation == 'multiply':
         return num1 * num2
-    elif operation == 'division':
-        if num2 == 0:
-            return "Error: Division by zero"
-        else:
+    elif operation == 'divide':
+        if num2 != 0:
             return num1 / num2
+        else:
+            return "Error: Division by zero"
     else:
         return "Error: Invalid operation"
