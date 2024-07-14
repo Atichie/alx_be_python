95680e17e490e2a6aaa7f53a2338821e3e46b91f
Atichie <atichie525@gmail.com>
2024-07-14 02:25:57 -0700

    Add bank_account.py script to calculate banking operations

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,45 @@
+class BankAccount:
+    def _init_(self, initial_balance = 0):
+        """
+        Initialize the BankAccount with an ptional initial balance
+        """
+
+    def deposit(self,amount):
+        """
+        deposit specified amount to the account
+        """
+        if amount > 0:
+            self.account_balance += amount
+            print(f"Deposited ${amount:2f}.")
+        else:
+            print("Deposit amount must be a positive.")
+
+    def withdraw(self, amount):
+        """
+        withdraw specified amount to the account
+        """
+        if amount > 0:
+            if self.account_balance >= amount:
+                self.account_balance -= amount
+                print(f"Withdraw ${amount:2f}.")
+                return True
+            else:
+                print("Insufficient fund.")
+                return False
+        else:
+            print("Withdrawn amount must be positive.")
+
+    def display_balance(self, amount):
+        """
+        display_balance should print the current balance in a user-friendly format
+        """
+        print(f"Current balance ${amount:2f}.")
+
+name = "main"
+if name == "_main_":
+    account = BankAccount(100)
+    account.deposit(50)
+    account.withdraw(20)
+    account.display_balance()
+
+
