f511487e5fe329fe48319ba79cf86d24e0018c46
Atichie <atichie525@gmail.com>
2024-07-14 07:35:14 -0700

    Update library_management.py

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,7 +1,5 @@
 class Book:
-    def __init__ (self, title, author):
-        self.title = title
-        self.author = author
+    def __init__(self):
         
     def check_out(self):
         if not self._is_checked_out:
