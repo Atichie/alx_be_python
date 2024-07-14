7e83f8539d21440fa6cc295fe4cfb7b160622d7a
Atichie <atichie525@gmail.com>
2024-07-14 03:26:32 -0700

    Update bank_account.py script to calaculate the deposit,withdrawand display_balance

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,43 +1,33 @@
 class BankAccount:
-    def _init_(self, initial_balance = 0):
-        self.account_balance = initial_balance
+    def _init_(self, account_balance=0.0):
+        self.account_balance = float(account_balance)
 
     def deposit(self,amount):
-        """
-        deposit specified amount to the account
-        """
-        if amount > 0:
-            self.account_balance += amount
-            print(f"Deposited ${amount:2f}.")
-        else:
-            print("Deposit amount must be a positive.")
+            self.account_balance += float(amount)
+            return self.account_balance
 
     def withdraw(self, amount):
-        """
-        withdraw specified amount to the account
-        """
-        if amount > 0:
-            if self.account_balance >= amount:
-                self.account_balance -= amount
-                print(f"Withdraw ${amount:2f}.")
-                return True
-            else:
-                print("Insufficient fund.")
-                return False
-        else:
-            print("Withdrawn amount must be positive.")
+        if 0 < amount <= self.account_balance:
+            self.account_balance -= float(amount)
+            return True
+        return False
 
-    def display_balance(self, amount):
-        """
-        display_balance should print the current balance in a user-friendly format
-        """
-        print(f"Current balance ${amount:2f}.")
+    def display_balance(self):
+        print(f"Current balance ${self.account_balance:2f}.")
 
 name = "main"
 if name == "_main_":
     account = BankAccount(100)
+    account.dispay_balance()
     account.deposit(50)
+    account.dispay_balance()
+
     account.withdraw(20)
+    account.dispay_balance()
+
+    success = account.withdraw(200)
+    print(f"Withdrawal successful: {success}")
+
     account.display_balance()
 
 
