7b541ccf5fef0ec4dffbbff991e39a74bfb5f50f
Atichie <atichie525@gmail.com>
2024-07-14 04:26:49 -0700

    Update robust_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -3,8 +3,10 @@ def safe_divide(numerator, denominator):
         num = float(numerator)
         denom = float(denominator)
 
-        result = num / denom
-        return f"The result of {num} divided by {denom} is {result,'.1f'}"
+        formatted_num = format(num, '.1f')
+        formatted_denom = format(denom, '.1f')
+        result = formatted_num / formatted_denom
+        return f"The result of {formatted_num} divided by {formatted_denom} is {result:1f}"
 
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
