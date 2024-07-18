0ee3e124fd9700091a0800808d9adca398cef449
Atichie <atichie525@gmail.com>
2024-07-18 04:04:09 -0700

    Update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -28,10 +28,7 @@ class Library:
         self.books = []
 
     def add_book(self, book):
-        if isinstance(book, Book):
-            self.books.append(book)
-        else:
-            print("Error: Only instances of Book or its subclasses can be added to the library")
+        self.books.append(book)
 
     def list_books(self):
         for book in self.books:
